8038c5eb0d8b69ac3317aeca19f4bffbf06da43a
Gerald <geraldpng@gmail.com>
2019-03-26 18:16:09 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Visualise flight legs
+
+A simple React app example using NextJS, Mapbox and Deck.gl to visualise all flights departing from Singapore. Read about it [here](https://medium.com/greatescapeco/visualizing-flight-legs-using-react-mapbox-and-deck-gl-18e22771d53e).
+
+## Installation
+
+1. Install dependancies
+
+```
+npm install
+```
+
+## Usage
+
+1. Run web server
+
+```
+npm run dev
+```
---
+++ pages/index.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+import DeckGL, { ArcLayer } from "deck.gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import arcs from "../data/arcs.json";
+
+const Index = () => {
+  const [viewport, setViewport] = useState({
+    height: "100%",
+    width: "100%"
+  });
+
+  return (
+    <div className="root">
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={newViewport => setViewport(newViewport)}
+        mapboxApiAccessToken="pk.eyJ1IjoiZ3BuZyIsImEiOiJjamZ0eXl3bGIzcWpoMndta3RmMG41dWw5In0.jMOkvFC0JBKLf1bOdaOfyA"
+        mapStyle="mapbox://styles/mapbox/light-v9"
+      >
+        <DeckGL
+          {...viewport}
+          layers={[
+            new ArcLayer({
+              id: "flight-arcs",
+              data: arcs,
+              getSourcePosition: d => d.source,
+              getTargetPosition: d => d.target,
+              getSourceColor: () => [255, 0, 0, 120],
+              getTargetColor: () => [0, 255, 0, 120],
+              getStrokeWidth: () => 2
+            })
+          ]}
+        />
+      </ReactMapGL>
+      <style jsx>
+        {`
+          .root {
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100vh;
+            width: 100vw;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default Index;
